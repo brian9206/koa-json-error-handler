df9f07c8d15467d7a64aa9ad38425b192f6136f4
Brian <brian9206@hotmail.com>
2018-05-17 17:21:56 +0800

    Fix 500 is not handled properly

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ module.exports = async function json_error_handler(ctx, next) {
         ctx.body = {
             result: false,
             reason: (() => {
-                if (status >= 500) {
-                    return `HTTP Error ${ctx.status}: ${HttpStatus.getStatusText(ctx.status)}`;
-                }
-    
                 if (err && err.message) {
                     return err.message.toString();
+                }
+
+                if (status >= 500) {
+                    return `HTTP Error ${status}: ${HttpStatus.getStatusText(status)}`;
                 }
     
                 return undefined;
